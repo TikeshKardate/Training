a85a8a2a6091ef42e17e9414139281e362ad8490
TikeshKardate <tikesh.kardate@gmail.com>
2025-01-07 12:53:58 +0530

    added addition functionality to calculator

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Calculator
+{
+public static void main (String [] args)
+{
+Scanner sc= new Scanner (System.in);
+System.out.println ("Enter First Number:");
+int a=sc.nextInt();
+System.out.println ("Enter Second Number:");
+int b=sc.nextInt();
+System.out.println ("Enter Name of the Operation:");
+char operation = sc.next().charAt(0);
+
+double result;
+
+if (operation == '+');
+{
+result = a + b;
+}
+else System.out.println("Error");
+System.out.println (result);
+
+
+
+}
+}
